1795d72800f216d9c931c3a724dcdae922b1beac
Anupam Ojha <anupamojha078@gmail.com>
2026-06-20 17:07:00 +0530

    Made an OpenAi Api call using Langchain

---
+++ Langchain_Models/1.LLMs/llm_demo.py
@@ -0,0 +1,8 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm=OpenAI(model_name="gpt-3.5-turbo-instruct")
+result=llm("What is the capital of France?")
+print(result)
